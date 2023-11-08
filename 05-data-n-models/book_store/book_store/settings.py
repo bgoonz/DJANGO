a2f2c6c180ba517751168ca621529fd22c1503a8
Bryan C Guner <bryan.guner@gmail.com>
2023-11-08 17:42:42 -0500

    about to insert data into data base... done with first migration

--- 05-data-n-models/book_store/book_store/settings.py
+++ 05-data-n-models/book_store/book_store/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "book_outlet"
+    "book_outlet",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
